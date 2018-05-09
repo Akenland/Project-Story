c4dc87676196d09c1342fb682a532f4fec1f61a3
Kyle Nanakdewa <KyleN@live.ca>
2018-05-09 04:23:57 -0400

    more journal test

--- src/com/kylenanakdewa/story/journal/JournalListener.java
+++ src/com/kylenanakdewa/story/journal/JournalListener.java
@@ -1,6 +1,7 @@
 package com.kylenanakdewa.story.journal;
 
 import com.kylenanakdewa.core.characters.players.PlayerCharacter;
+import com.kylenanakdewa.core.common.Utils;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -8,6 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 
 /**
  * JournalListener
@@ -19,13 +21,15 @@ public final class JournalListener implements Listener {
 	 * @return true if item is a journal
 	 */
 	public static boolean isJournal(ItemStack item){
-		return item!=null && item.getType().equals(Material.WRITTEN_BOOK) && item.getItemMeta().getDisplayName().equals(ChatColor.DARK_PURPLE+"Journal");
+		return item!=null && item.getType().equals(Material.WRITTEN_BOOK) && ((BookMeta)item.getItemMeta()).getTitle().equals(ChatColor.DARK_PURPLE+"Journal");
 	}
 
 
 	public void onJournalOpen(PlayerInteractEvent event){
 		ItemStack item = event.getItem();
 		if(isJournal(item)){
+
+			Utils.notifyAdmins("Found valid journal");
 
 			ItemStack journal = new Journal(PlayerCharacter.getCharacter(event.getPlayer())).getJournalItem();
 
